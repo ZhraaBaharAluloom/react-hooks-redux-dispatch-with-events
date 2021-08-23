ffa9061a419375635ef75da1d625af7dcb2b663e
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-23 16:22:56 -0700

    fix reducer file

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,30 @@
 // add code snippets from README
+let state = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+reducer({ count: 0 }, { type: "counter/increment" });
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+dispatch({ type: "@@INIT" });
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
